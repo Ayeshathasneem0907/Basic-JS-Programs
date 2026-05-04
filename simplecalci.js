c843a13c3f21127459a274f659f4a8045836c34e
Ayeshathasneeem0907 <ayeshathasneem0907@gmail.com>
2026-05-04 14:23:14 +0530

    Text file Added

---
+++ simplecalci.js
@@ -0,0 +1,18 @@
+let num1 = 4
+let num2 = 5
+let operator = "+"
+
+switch(operator)
+{
+    case "+":
+        console.log(num1 + num2)
+    
+    case "-":
+        console.log(num2 - num1)
+    
+    case "*":
+        console.log(num1 * num2)
+
+    case "/":
+        console.log(num1 / 2)
+}
